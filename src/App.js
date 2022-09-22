e76b8d5c0a54b1ed4183aab96dc33bbf9b79df85
Aditya Singh <emails4adi@gmail.com>
2022-09-23 05:11:17 +0530

    changrs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./style/App.css";
 import English from "./sites/English.js";
-import Hindi from "./sites/Hindi.js";
-import Urdu from "./sites/Urdu.js";
-import Russian from "./sites/Russian.js";
-import Spanish from "./sites/Spanish.js";
+// import Hindi from "./sites/Hindi.js";
+// import Urdu from "./sites/Urdu.js";
+// import Russian from "./sites/Russian.js";
+// import Spanish from "./sites/Spanish.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,13 +13,13 @@ function App() {
         <Routes>
           <Route path="/" element={<English />} />
 
-          <Route path="/Hindi" element={<Hindi />} />
+          {/* <Route path="/Hindi" element={<Hindi />} />
 
           <Route path="/Urdu" element={<Urdu />} />
 
           <Route path="/Russian" element={<Russian />} />
 
-          <Route path="/Spanish" element={<Spanish />} />
+          <Route path="/Spanish" element={<Spanish />} /> */}
         </Routes>
       </Router>
     </div>
