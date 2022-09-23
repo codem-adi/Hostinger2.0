2888f3f1c9136afda2c6fba4fc39bf3e0dd0503f
Aditya Singh <emails4adi@gmail.com>
2022-09-23 05:48:26 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "./style/App.css";
 import English from "./sites/English.js";
-// import Hindi from "./sites/Hindi.js";
-// import Urdu from "./sites/Urdu.js";
-// import Russian from "./sites/Russian.js";
-// import Spanish from "./sites/Spanish.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hindi from "./sites/Hindi.js";
+import Urdu from "./sites/Urdu.js";
+import Russian from "./sites/Russian.js";
+import Spanish from "./sites/Spanish.js";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<English />} />
+      <Routes>
+        <Route exact path="/" element={<English />} />
 
-          {/* <Route path="/Hindi" element={<Hindi />} />
+        <Route exact  path="/Hindi"  element={<Hindi />} />
 
-          <Route path="/Urdu" element={<Urdu />} />
+          <Route  exact path="/Urdu"  element={<Urdu />} />
 
-          <Route path="/Russian" element={<Russian />} />
+          <Route exact  path="/Russian"  element={<Russian />} />
 
-          <Route path="/Spanish" element={<Spanish />} /> */}
-        </Routes>
-      </Router>
+          <Route  exact path="/Spanish"  element={<Spanish />} />
+          <Redirect t0="/"
+      </Routes>
     </div>
   );
 }
